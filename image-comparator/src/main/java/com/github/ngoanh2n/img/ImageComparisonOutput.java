5899e514d44ed4f9e7b135c4c8055c7ac72169d3
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 21:06:20 +0700

    Implement ImageComparisonOutput

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonOutput.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonOutput.java
@@ -1,4 +1,26 @@
 package com.github.ngoanh2n.img;
 
-public class ImageComparisonOutput {
+import com.github.ngoanh2n.Commons;
+import com.github.ngoanh2n.RuntimeError;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class ImageComparisonOutput implements ImageComparisonVisitor {
+    @Override
+    public void comparisonFinished(ImageComparisonOptions options, BufferedImage exp, BufferedImage act, ImageComparisonResult result) {
+        if (options.resultOptions().writeOutput()) {
+            String fileName = Commons.timestamp() + ".png";
+            Path location = options.resultOptions().location();
+            Path output = Commons.createDir(location).resolve(fileName);
+
+            try {
+                ImageIO.write(result.getDiffImage(), "png", output.toFile());
+            } catch (IOException e) {
+                throw new RuntimeError("Could not write diff image: " + output, e);
+            }
+        }
+    }
 }
