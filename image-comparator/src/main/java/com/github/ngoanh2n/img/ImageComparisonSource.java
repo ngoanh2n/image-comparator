6bb529b0e1ede6b7d66d276be39e24281d36e314
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 21:09:50 +0700

    Implement ImageComparisonSource

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonSource.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonSource.java
@@ -1,4 +1,37 @@
 package com.github.ngoanh2n.img;
 
-public class ImageComparisonSource {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.HashSet;
+import java.util.Set;
+
+class ImageComparisonSource {
+    private final BufferedImage image;
+    private final Set<Rectangle> rectanglesToIgnore;
+    private final Set<Rectangle> rectanglesToCompare;
+
+    ImageComparisonSource(BufferedImage image) {
+        this.image = image;
+        this.rectanglesToIgnore = new HashSet<>();
+        this.rectanglesToCompare = new HashSet<>();
+        this.rectanglesToCompare.add(this.getRectangle());
+    }
+
+    //-------------------------------------------------------------------------------//
+
+    BufferedImage getImage() {
+        return image;
+    }
+
+    Rectangle getRectangle() {
+        return new Rectangle(image.getWidth(), image.getHeight());
+    }
+
+    Set<Rectangle> getRectanglesToIgnore() {
+        return rectanglesToIgnore;
+    }
+
+    Set<Rectangle> getRectanglesToCompare() {
+        return rectanglesToCompare;
+    }
 }
