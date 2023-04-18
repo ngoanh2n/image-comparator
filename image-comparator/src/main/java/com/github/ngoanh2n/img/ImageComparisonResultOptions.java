a0e390cc9c8e3b205f76b59effd3c475565c807b
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 20:54:28 +0700

    Implement ImageComparisonResultOptions

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonResultOptions.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonResultOptions.java
@@ -1,4 +1,60 @@
 package com.github.ngoanh2n.img;
 
-public class ImageComparisonResultOptions {
+import com.google.common.base.Preconditions;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@ParametersAreNonnullByDefault
+public interface ImageComparisonResultOptions {
+    static Builder builder() {
+        return new Builder();
+    }
+
+    static ImageComparisonResultOptions defaults() {
+        return builder().build();
+    }
+
+    //-------------------------------------------------------------------------------//
+
+    Path location();
+
+    boolean writeOutput();
+
+    //===============================================================================//
+
+    final class Builder {
+        private Path location;
+        private boolean writeOutput;
+
+        private Builder() {
+            writeOutput = true;
+            location = Paths.get("build/ngoanh2n/img");
+        }
+
+        public Builder setLocation(Path value) {
+            location = Preconditions.checkNotNull(value, "path cannot not be null");
+            return this;
+        }
+
+        public Builder writeOutput(boolean enabled) {
+            writeOutput = enabled;
+            return this;
+        }
+
+        public ImageComparisonResultOptions build() {
+            return new ImageComparisonResultOptions() {
+                @Override
+                public Path location() {
+                    return location;
+                }
+
+                @Override
+                public boolean writeOutput() {
+                    return writeOutput;
+                }
+            };
+        }
+    }
 }
