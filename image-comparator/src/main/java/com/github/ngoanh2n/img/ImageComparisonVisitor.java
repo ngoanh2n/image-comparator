199892b7ef86e336bb98bb924ecca5ba140ed93f
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 20:58:51 +0700

    Implement ImageComparisonVisitor

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonVisitor.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparisonVisitor.java
@@ -1,4 +1,9 @@
 package com.github.ngoanh2n.img;
 
-public class ImageComparisonVisitor {
+import java.awt.image.BufferedImage;
+
+public interface ImageComparisonVisitor {
+    default void comparisonStarted(ImageComparisonOptions options, BufferedImage exp, BufferedImage act) {/**/}
+
+    default void comparisonFinished(ImageComparisonOptions options, BufferedImage exp, BufferedImage act, ImageComparisonResult result) {/**/}
 }
