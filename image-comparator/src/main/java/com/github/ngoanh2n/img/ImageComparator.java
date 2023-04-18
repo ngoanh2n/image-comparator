c9d9ef2f1d897f37c464df85051c17c53cec51d0
ngoanh2n <ngoanh2n@gmail.com>
2023-04-18 21:03:20 +0700

    Create main comparison flow

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparator.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageComparator.java
@@ -1,4 +1,35 @@
 package com.github.ngoanh2n.img;
 
-public class ImageComparator {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.awt.image.BufferedImage;
+
+@ParametersAreNonnullByDefault
+public final class ImageComparator {
+    private final static Logger log = LoggerFactory.getLogger(ImageComparator.class);
+    private final BufferedImage exp;
+    private final BufferedImage act;
+    private final ImageComparisonOptions options;
+
+    private ImageComparator(BufferedImage exp, BufferedImage act, ImageComparisonOptions options) {
+        this.exp = exp;
+        this.act = act;
+        this.options = options;
+    }
+
+    public static ImageComparisonResult compare(BufferedImage exp, BufferedImage act) {
+        return compare(exp, act, ImageComparisonOptions.defaults());
+    }
+
+    public static ImageComparisonResult compare(BufferedImage exp, BufferedImage act, ImageComparisonOptions options) {
+        return new ImageComparator(exp, act, options).compare();
+    }
+
+    //-------------------------------------------------------------------------------//
+
+    private ImageComparisonResult compare() {
+        return null;
+    }
 }
