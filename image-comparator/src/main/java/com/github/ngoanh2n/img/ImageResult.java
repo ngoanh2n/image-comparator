59df13f118093f4e61eae8e4221521b3b03f7b4e
ngoanh2n <ngoanh2n@gmail.com>
2023-04-19 20:59:16 +0700

    Implement ImageResult

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageResult.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageResult.java
@@ -1,4 +1,52 @@
 package com.github.ngoanh2n.img;
 
-public class ImageResult {
+import java.awt.image.BufferedImage;
+
+class ImageResult implements ImageComparisonResult {
+    private final int diffSize;
+    private final BufferedImage diffImage;
+    private final double allowedDeviation;
+    private final double currentDeviation;
+
+    ImageResult(ImageComparisonSources comparisonSources, ImageComparisonOptions options) {
+        this.diffSize = comparisonSources.getSize();
+        this.diffImage = comparisonSources.getImage();
+        this.allowedDeviation = options.deviation();
+
+        int width = comparisonSources.getMaxWidth();
+        int height = comparisonSources.getMaxHeight();
+        this.currentDeviation = diffSize / (double) (width * height);
+    }
+
+    //-------------------------------------------------------------------------------//
+
+    @Override
+    public boolean isDifferent() {
+        return !(currentDeviation == 0 || currentDeviation <= allowedDeviation);
+    }
+
+    @Override
+    public int getDiffSize() {
+        return diffSize;
+    }
+
+    @Override
+    public BufferedImage getDiffImage() {
+        return diffImage;
+    }
+
+    @Override
+    public double getAllowedDeviation() {
+        return allowedDeviation;
+    }
+
+    @Override
+    public double getCurrentDeviation() {
+        return currentDeviation;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("deviation=[allowed:%s, current:%s]", allowedDeviation, currentDeviation);
+    }
 }
