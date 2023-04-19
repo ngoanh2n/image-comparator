fb0542617b5941ef2079333e412e538f5db03572
ngoanh2n <ngoanh2n@gmail.com>
2023-04-19 21:04:30 +0700

    Add the test

---
+++ image-comparator/src/test/java/com/github/ngoanh2n/img/ImageComparatorTest.java
@@ -0,0 +1,35 @@
+package com.github.ngoanh2n.img;
+
+import com.github.ngoanh2n.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageComparatorTest {
+    @Test
+    void equal() throws IOException {
+        BufferedImage exp = getImage("com/github/ngoanh2n/img/exp/image.jpg");
+        BufferedImage act = getImage("com/github/ngoanh2n/img/exp/image.jpg");
+
+        ImageComparisonResult result = ImageComparator.compare(exp, act);
+        Assertions.assertFalse(result.isDifferent());
+    }
+
+    @Test
+    void different() throws IOException {
+        BufferedImage exp = getImage("com/github/ngoanh2n/img/exp/image.jpg");
+        BufferedImage act = getImage("com/github/ngoanh2n/img/act/image.jpg");
+
+        ImageComparisonResult result = ImageComparator.compare(exp, act);
+        Assertions.assertTrue(result.isDifferent());
+    }
+
+    private BufferedImage getImage(String resourceName) throws IOException {
+        File file = Resource.getFile(resourceName);
+        return ImageIO.read(file);
+    }
+}
