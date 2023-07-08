be52e17d85b0f208df1fa5c715787e5218af3f03
ngoanh2n <ngoanh2n@gmail.com>
2023-07-08 13:06:52 +0700

    [img] Declare for ImageBulkResult

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageBulkResult.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageBulkResult.java
@@ -1,4 +1,27 @@
 package com.github.ngoanh2n.img;
 
-public class ImageBulkResult {
+import java.util.ArrayList;
+import java.util.List;
+
+class ImageBulkResult implements ImageBulkComparisonResult {
+    private final List<ImageComparisonResult> results = new ArrayList<>();
+
+    void put(ImageComparisonResult result) {
+        results.add(result);
+    }
+
+    @Override
+    public boolean hasDiff() {
+        return getDiffTotal() > 0;
+    }
+
+    @Override
+    public int getDiffTotal() {
+        return (int) results.stream().filter(ImageComparisonResult::hasDiff).count();
+    }
+
+    @Override
+    public List<ImageComparisonResult> getDiffResults() {
+        return results;
+    }
 }
