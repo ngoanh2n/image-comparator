3ba99472df18d292827f7f709f9bfd97a57a9a04
ngoanh2n <ngoanh2n@gmail.com>
2023-07-08 15:16:59 +0700

    [javadoc] Add description for ImagBulkResult

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageBulkResult.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageBulkResult.java
@@ -3,6 +3,16 @@ package com.github.ngoanh2n.img;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <em>Repository:</em>
+ * <ul>
+ *     <li><em>GitHub: <a href="https://github.com/ngoanh2n/image-comparator">ngoanh2n/image-comparator</a></em></li>
+ *     <li><em>Maven: <a href="https://mvnrepository.com/artifact/com.github.ngoanh2n/image-comparator">com.github.ngoanh2n:image-comparator</a></em></li>
+ * </ul>
+ *
+ * @author ngoanh2n
+ * @since 2021
+ */
 class ImageBulkResult implements ImageBulkComparisonResult {
     private final List<ImageComparisonResult> results = new ArrayList<>();
 
