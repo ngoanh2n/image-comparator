4068ce779c803b4a976186876fee12d1d3cd4b82
ngoanh2n <ngoanh2n@gmail.com>
2023-07-08 22:47:35 +0700

    [img] Remove ignored rectangles

--- image-comparator/src/main/java/com/github/ngoanh2n/img/ImageSource.java
+++ image-comparator/src/main/java/com/github/ngoanh2n/img/ImageSource.java
@@ -1,7 +1,6 @@
 package com.github.ngoanh2n.img;
 
 import java.awt.*;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -19,34 +18,33 @@ class ImageSource {
     private final Set<Rectangle> rectangles;
     private final Rectangle minRectangle;
 
-    ImageSource(Set<Rectangle> rectangles) {
-        this.rectangles = rectangles;
-        this.minRectangle = getMinRectangle();
+    ImageSource(ImageComparisonSource diffSource) {
+        this.rectangles = getRectangles(diffSource);
+        this.minRectangle = getMinRectangle(rectangles);
     }
 
     //-------------------------------------------------------------------------------//
 
-    static ImageSource createIgnore(ImageComparisonSource exp, ImageComparisonSource act) {
-        Set<Rectangle> rectangles = new HashSet<>();
-        Set<Rectangle> expRectangles = exp.getRectanglesToIgnore();
-        Set<Rectangle> actRectangles = act.getRectanglesToIgnore();
-
-        for (Rectangle expRectangle : expRectangles) {
-            for (Rectangle actRectangle : actRectangles) {
-                Rectangle rectangle = expRectangle.intersection(actRectangle);
-                if (!rectangle.isEmpty()) {
-                    rectangles.add(rectangle);
-                }
-            }
-        }
-        return new ImageSource(rectangles);
+    private static Set<Rectangle> getRectangles(ImageComparisonSource diffSource) {
+        Set<Rectangle> rectangles = new LinkedHashSet<>();
+        rectangles.add(diffSource.getExpRectangle());
+        rectangles.add(diffSource.getActRectangle());
+        return rectangles;
     }
 
-    static ImageSource createDiff(ImageComparisonSource exp, ImageComparisonSource act) {
-        Set<Rectangle> rectangles = new LinkedHashSet<>();
-        rectangles.addAll(exp.getRectanglesToCompare());
-        rectangles.addAll(act.getRectanglesToCompare());
-        return new ImageSource(rectangles);
+    private static Rectangle getMinRectangle(Set<Rectangle> rectangles) {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = 0;
+        int maxY = 0;
+
+        for (Rectangle rectangle : rectangles) {
+            minX = Math.min(minX, (int) rectangle.getMinX());
+            minY = Math.min(minY, (int) rectangle.getMinY());
+            maxX = Math.max(maxX, (int) rectangle.getMaxX());
+            maxY = Math.max(maxY, (int) rectangle.getMaxY());
+        }
+        return new Rectangle(minX, minY, maxX - minX, maxY - minY);
     }
 
     //-------------------------------------------------------------------------------//
@@ -64,20 +62,5 @@ class ImageSource {
             return false;
         }
         return false;
-    }
-
-    private Rectangle getMinRectangle() {
-        int minX = Integer.MAX_VALUE;
-        int minY = Integer.MAX_VALUE;
-        int maxX = 0;
-        int maxY = 0;
-
-        for (Rectangle rectangle : rectangles) {
-            minX = Math.min(minX, (int) rectangle.getMinX());
-            minY = Math.min(minY, (int) rectangle.getMinY());
-            maxX = Math.max(maxX, (int) rectangle.getMaxX());
-            maxY = Math.max(maxY, (int) rectangle.getMaxY());
-        }
-        return new Rectangle(minX, minY, maxX - minX, maxY - minY);
     }
 }
